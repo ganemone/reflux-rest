f969b0d7045934323b8a7b3e171c31019241e378
Giancarlo Anemone <ganemone@gmail.com>
2015-02-25 13:43:36 -0500

    Add tests for createModel.toJSON

--- tests/testCreateModel.js
+++ tests/testCreateModel.js
@@ -2,24 +2,32 @@ var assert = require('chai').assert;
 var createModel = require('../js/createModel');
 
 describe('createModel', function () {
-  it('should create a constructor correctly', function () {
-    var Model = createModel({
-      name: 'Model',
-      structure: {
-        username: {
-          'required': true,
-          'type': String
-        },
-        firstName: {
-          'required': false,
-        }
+  var Model = createModel({
+    name: 'Model',
+    structure: {
+      username: {
+        'required': true,
+        'type': String
+      },
+      firstName: {
+        'required': false,
       }
-    });
-    var testNewModel = new Model({
+    }
+  });
+  var testNewModel = new Model({
+    username: 'ganemone',
+    firstName: 'somename'
+  });
+  it('should create a constructor correctly', function () {
+    assert.equal(testNewModel.username, 'ganemone');
+    assert.equal(testNewModel.firstName, 'somename');
+  });
+  it('should have a toJSON method that works correctly', function () {
+    assert.deepEqual(testNewModel.toJSON(), {
+      type: 'Model',
       username: 'ganemone',
       firstName: 'somename'
     });
-    assert.equal(testNewModel.username, 'ganemone');
-    assert.equal(testNewModel.firstName, 'somename');
+    assert.deepEqual(testNewModel.toJSON(), testNewModel.__toJSON())
   });
 });
